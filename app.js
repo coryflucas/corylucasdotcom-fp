d431cd14f698ef0a19e28f6828802eb00c3a40f8
Cory Lucas <cory@corylucas.com>
2013-09-02 17:28:48 -0400

    Use gravitar for fav icon

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -14,24 +13,28 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use('/blog/', fp.blog(
-  {
-    postsDir: __dirname + '/posts',
-    listTemplate: 'blogListTemplate',
-    postTemplate: 'blogPostTemplate'
-  }));
+    {
+        postsDir: __dirname + '/posts',
+        listTemplate: 'blogListTemplate',
+        postTemplate: 'blogPostTemplate'
+    }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var request = require('request');
+app.get('/favicon.ico', function (req, res) {
+    request('http://gravatar.com/avatar/8f04ed154a6a276594899964a567a885.png?s=16').pipe(res);
+})
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
